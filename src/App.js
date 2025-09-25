834cad24ac03ad1aa580c08d947b05ff99fa325c
IscanderB <baizakov.iscander@gmail.com>
2025-09-25 16:22:42 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1111,7 +1111,7 @@ const ProjectStatusDashboard = () => {
               src="/zigert-logo.png"
               alt="Zigert Logo"
               style={{
-                height: '60px',
+                height: '100px',
                 width: 'auto',
                 filter: 'grayscale(0)',
                 transition: 'all 0.3s ease'
